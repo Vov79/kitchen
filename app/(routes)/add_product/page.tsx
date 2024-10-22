65d917d401e202d6c6467517b99123c3a650a9ec
Vladimir <iancovoi.vladimir@gmail.com>
2024-10-22 16:33:12 +0300

    Sec

--- app/(routes)/add_product/page.tsx
+++ app/(routes)/add_product/page.tsx
@@ -4,7 +4,7 @@ import CreateItem from '../../components/CreateItem';
 export default function Product() {
     return(
         <div>
-            <h1>Create Item</h1>
+            <h1>Create</h1>
                   <CreateItem />
             
         </div>
