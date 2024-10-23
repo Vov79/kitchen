acef883c5e3b3a30b12ac85af2c0d64df3abcfd5
Itritiac <itritiac@gmail.com>
2024-10-23 15:21:04 +0300

    transfer desing

--- app/layout.tsx
+++ app/layout.tsx
@@ -3,10 +3,6 @@ import localFont from "next/font/local";
 import "./globals.scss";
 import Link from "next/link";
 import Header from "./components/Header.jsx";
-import Intro from "./components/Intro.jsx";
-import Recomended from "./components/Recomended.jsx";
-import Info from "./components/Info.jsx";
-import Testimonials from "./components/Testimonials.jsx";
 
 export default function RootLayout({
   children,
@@ -15,12 +11,8 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body className="app">
-        <Header/>
-        <Intro/>
-        <Recomended/>
-        <Info/>
-        <Testimonials/>
+      <body>
+      <Header/>
         {children}
       </body>
     </html>
--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,20 @@
 import Link from 'next/link';
+import Header from "./components/Header.jsx";
+import Intro from "./components/Intro.jsx";
+import Recomended from "./components/Recomended.jsx";
+import Info from "./components/Info.jsx";
+import Testimonials from "./components/Testimonials.jsx";
+import About from "./components/About.jsx";
 
 
 export default function Home() {
   return (
-    <div>
-      <Link href={"/"}>Home</Link>
+    <div className='app'>
+        <Intro/>
+        <Recomended/>
+        <Info/>
+        <Testimonials/>
+        <About/>
     </div>
   );
 }
