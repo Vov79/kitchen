e4609f1d986365cb049a955dd93052cb24693e98
David Gherlovanu <dgherlovanu098890@gmail.com>
2024-10-29 10:46:35 +0200

    Refactor import statements and add metadata to pages

--- app/(routes)/showrooms/page.tsx
+++ app/(routes)/showrooms/page.tsx
@@ -1,10 +1,26 @@
-import CreateItem from '../../components/CreateItem';
+import CreateItem from "../../components/CreateItem";
 
+import type { Metadata } from "next";
 
+export const metadata: Metadata = {
+  title: "Croitoru | Showroom-uri",
+  description: "Croitoru - Bucătării la comandă cu design italian",
+  keywords: "bucătării, mobilă, design, italian, comandă, croitoru",
+  openGraph: {
+    images: [
+      {
+        url: "/images/og-image.png",
+        width: 800,
+        height: 600,
+        alt: "Croitoru",
+      },
+    ],
+  },
+};
 export default function Showrooms() {
-    return(
-        <div>
-            <h1>Product Page</h1>
-        </div>
-    );
+  return (
+    <div>
+      <h1>Product Page</h1>
+    </div>
+  );
 }
