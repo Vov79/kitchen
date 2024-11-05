d8f17c81756bea6e367310ed094dbb85ff5dd2ea
Vladimir <bucatari.croitoru@gmail.com>
2024-11-05 16:00:21 +0200

    seo

--- app/layout.tsx
+++ app/layout.tsx
@@ -6,6 +6,7 @@ import Header from "./components/Header.jsx";
 import Footer from "./components/Footer.jsx";
 import { ToastContainer, toast } from "react-toastify";
 import "react-toastify/dist/ReactToastify.css";
+import Script from "next/script";
 
 export default function RootLayout({
   children,
@@ -19,19 +20,33 @@ export default function RootLayout({
     >
       <head>
         <link rel="icon" href="/images/favicon.ico" sizes="any" />
-        {/*  Google Tag Manager  */}
-<script>(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
-new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
-j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
-'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
-})(window,document,'script','dataLayer','GTM-N86LVST');</script>
-{/*  End Google Tag Manager  */}
+       {/* Google Tag Manager */}
+       <Script
+          id="gtm-script"
+          strategy="afterInteractive"
+          dangerouslySetInnerHTML={{
+            __html: `
+              (function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+              new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+              j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+              'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+              })(window,document,'script','dataLayer','GTM-N86LVST');
+            `,
+          }}
+        />
+        {/* End Google Tag Manager */}
       </head>
       <body>
-       {/* Google Tag Manager (noscript)  */}
-<noscript><iframe src="https://www.googletagmanager.com/ns.html?id=GTM-N86LVST"
-height="0" width="0" style={{display: "none", visibility: "hidden"}}></iframe></noscript>
- {/* End Google Tag Manager (noscript)  */}
+        {/* Google Tag Manager (noscript) */}
+        <noscript>
+          <iframe
+            src="https://www.googletagmanager.com/ns.html?id=GTM-N86LVST"
+            height="0"
+            width="0"
+            style={{ display: "none", visibility: "hidden" }}
+          ></iframe>
+        </noscript>
+        {/* End Google Tag Manager (noscript) */}
         <Header />
         {children}
         <Footer />
